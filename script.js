50acd606ea004746431c5163bff17bdffb208107
itsr0ya <akbariroya374@gmail.com>
2026-03-23 09:19:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
 
-window.addEventListener('scroll', () => {
-  document.getElementById('nav-inner')
-    .classList.toggle('scrolled', window.scrollY > 0);
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("main-navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
